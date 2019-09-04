258d2599502ce1965eae4409ec4fcf4f45a1796a
Nathanbutterfield <54914767+Nathanbutterfield@users.noreply.github.com>
2019-09-04 15:27:46 -0600

    Update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyAk8qDfkkOPmZgGfcgGmk8QnP2FLn8VCsc",
+  authDomain: "authentication-d4bf8.firebaseapp.com",
+  databaseURL: "https://authentication-d4bf8.firebaseio.com",
+  projectId: "authentication-d4bf8",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "59747142167",
+  appId: "1:59747142167:web:d46bb407c156fdc3146a52"
 };
 
 firebase.initializeApp(config);
