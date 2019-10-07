1e9541cfdb0241414fb37bf9b6af8a8e4f98a9d8
Nathanbutterfield <54914767+Nathanbutterfield@users.noreply.github.com>
2019-10-07 00:17:32 -0600

    actual final auth app

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyAk8qDfkkOPmZgGfcgGmk8QnP2FLn8VCsc",
-  authDomain: "authentication-d4bf8.firebaseapp.com",
-  databaseURL: "https://authentication-d4bf8.firebaseio.com",
-  projectId: "authentication-d4bf8",
+  apiKey: "AIzaSyD22ffuYjuvTPm1u33Y64qbmgmE-fi8m9o",
+  authDomain: "authentication2-a2c4b.firebaseapp.com",
+  databaseURL: "https://authentication2-a2c4b.firebaseio.com",
+  projectId: "authentication2-a2c4b",
   storageBucket: "",
-  messagingSenderId: "59747142167",
-  appId: "1:59747142167:web:d46bb407c156fdc3146a52"
+  messagingSenderId: "365655359740",
+  appId: "1:365655359740:web:b34c6c42488fcdb0cdd779"
 };
 
 firebase.initializeApp(config);
